727bf15a5836da11e034fb90deb76c92df769a55
Bastian Ruppert <info@bastian-ruppert.de>
2011-09-21 20:17:36 +0200

    pack.sh initial checkin
    Signed-off-by: Bastian Ruppert <info@bastian-ruppert.de>

---
+++ pack.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+mkdir cap_test
+cp ../../stage/x86/usr/local/lib/libdsp_jpeg.so.1.0 cap_test/
+cp cap cap_test
+cp cap.conf cap_test
+cp aispec.ttf cap_test
+cp LiberationMono-Regular.ttf cap_test
+cp v4l_capture.o cap_test
+cp start_cap.sh cap_test
+cp readme cap_test
+cp appcontroller.sh cap_test
+
+
+tar cfz `date +"%y%m%d%H%M%S"`_cap.tar.gz cap_test
+rm -R cap_test
