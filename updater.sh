59e871646c42934a5d29512b966fb30bfa963d77
Bastian Ruppert <info@bastian-ruppert.de>
2011-10-29 16:50:47 +0200

    updater.sh stage_check_current_version begonnen
    Signed-off-by: Bastian Ruppert <info@bastian-ruppert.de>

--- updater.sh
+++ updater.sh
@@ -6,15 +6,11 @@
 
 # Global definitions
 VERSION="2.0"
-INSTALL_DIR="/opt/"
+#INSTALL_DIR="/opt/"
 TMP_USBSTICK=""
 
-
-UBITOOLS="/opt/app/mtd-utils/ubi-utils"
 #set -x
 
-#cp -f updater.sh /home/sewerin/denx/arm/etc/.
-#./updater.sh /usr/EA20IMG_20110405151042.tar validate copy flipboot
 
 die()
 {
@@ -25,26 +21,6 @@ die()
 
 # Helper functions
 
-# Find active set from U-Boot environment
-read_activeset()
-{
-    # Only read active set once
-    if [ -n "${ACTIVE_SET}" ]; then
-		 return
-    fi
-
-    ACTIVE_SET=`fw_printenv as || echo as=3`
-    ACTIVE_SET=`echo ${ACTIVE_SET} | sed 's/^.\+=//'`
-    echo "active set is ${ACTIVE_SET}"
-
-    if [ "${ACTIVE_SET}" = "3" ]; then
-	MTD_ROOTFS="4"
-    else
-	MTD_ROOTFS="3"
-    fi
-    echo MTD_ROOTFS is ${MTD_ROOTFS}
-}
-
 # Implementation of individual stages
 stage_validate()
 {
@@ -52,98 +28,29 @@ stage_validate()
     echo "- validate"
     tar xOf ${update_file} ${UPD_MD5SUMS} > ${TMP_MD5SUMS}
     for file in ${COMPONENTS}; do
-		 tar tf ${update_file} | grep -q $file || die "needed component '$file' not found in ${update_file}"
-		 exp_sum=`awk '{ if ($2 == "'${file}'") print $1; }' ${TMP_MD5SUMS}`
-		 sum=`tar xOf ${update_file} ${file} | md5sum | awk '{ print $1; }'`
-		 if [ "$exp_sum" != "$sum" ]; then
-		     die "md5sum mismatch for file '$file'"
-		 fi
+        tar tf ${update_file} | grep -q $file || die "needed component '$file' not found in ${update_file}"
+        exp_sum=`awk '{ if ($2 == "'${file}'") print $1; }' ${TMP_MD5SUMS}`
+        sum=`tar xOf ${update_file} ${file} | md5sum | awk '{ print $1; }'`
+        if [ "$exp_sum" != "$sum" ]; then
+            die "md5sum mismatch for file '$file'"
+        fi
     done
     exit 0;
 }
 
-stage_copy()
-{
-    echo "- copy"
-    read_activeset
 
-    echo "read Image Size"
-    #TMP_IMAGESIZE=`tar xOf ${update_file} ${UPD_IMAGESIZE} | awk '{ print $1; }'`
-    TMP_IMAGESIZE=`tar tvf ${update_file} | awk '/'"${UPD_ROOTFS}"'/ { print $3; }'`
-    echo "ImageSize = ${TMP_IMAGESIZE}"
-    echo "copy Image to ${MTD_ROOTFS}"
-    tar xOf ${update_file} ${UPD_ROOTFS} | ${UBITOOLS}/./ubiformat /dev/mtd$MTD_ROOTFS -q -f - -S $TMP_IMAGESIZE
-
-    RETURNVAL=$?
-    exit $RETURNVAL
-}
-
-stage_verify()
-{
-    echo "- verify"
-    read_activeset
-
-${UBITOOLS}/./ubiattach /dev/ubi_ctrl -m ${MTD_ROOTFS} -d 5
-#./ubidetach -d 5
-if [ $? -ne 0 ]
-    then
-    echo "Fehler ubiattach 0"
-    RETURNVAL=$?
-    exit $RETURNVAL
-fi
-
-
-#    echo "dd if=${MTD_KERNEL} of=${FIFO} &"
-#    echo "BGJOB=$!"
-#    echo "tar xOf ${update_file} ${UPD_KERNEL} | cmp - ${FIFO}"
-#    echo "kill $BGJOB"
-#    dd if=/dev/mtd${MTD_ROOTFS} of=${FIFO} &
-    dd if=/dev/ubi5_0 of=${FIFO} &
-    BPID=$!
-    tar xOf ${update_file} ${UPD_ROOTFS} | cmp - ${FIFO} 2>&1
-    RETURNVAL=$?
-    exit $RETURNVAL
-}
-
-
-stage_flipboot()
-{
-    echo "- flipboot"
-    read_activeset
-    if [ "${ACTIVE_SET}" = "3" ]; then
-		 ACTIVE_SET="4"
-    else
-		 ACTIVE_SET="3"
-    fi
-    echo "fw_setenv as ${ACTIVE_SET}"
-    fw_setenv as ${ACTIVE_SET}
-    RETURNVAL=$?
-    exit $RETURNVAL
-}
-
-stage_search_stick2()
-{
-for file in "/dev/disk/by-label"
-do
-#echo $file
-#echo
-  ls -l $file | awk '{ print $8 "         file size: " $5 }'  # Print 2 fields.
-#  whatis `basename $file`   # File info.
-  # Note that the whatis database needs to have been set up for this to work.
-  # To do this, as root run /usr/bin/makewhatis.
-#  echo
-done 
-}
 
 
 REGEXP='[0-9]\{12\}_capmb.tar.gz$'
+EXEX_DIR_LINK="capmb"
 UPD_FILE_LEN=25
 UPD_DIR_LEN=18
 COMPONENTS="cap cap.conf v4l_capture.o appcontroller.sh"
 UPD_FILE="" #/media/SWAP/111016114939_capmb.tar.gz"
 UPD_DISK="/media/"
 UPD_DIR_NAME=""
-UPDATE_STEPS="stage_search_stick stage_search_updatefile stage_check_updatefile"
+#UPDATE_STEPS="stage_search_stick stage_search_updatefile stage_check_updatefile"
+UPDATE_STEPS="stage_check_current_version"
 
 stage_update()
 {
@@ -153,7 +60,7 @@ stage_update()
 	eval "$func"
 	if [[ $? -ne 0 ]]
 	then
-	    echo "no update done, $func retuned value $returnval"
+	    echo "no update done, $func returned non zero value"
 	    return 1
 	fi
     done
@@ -184,6 +91,34 @@ retval=0
 	tar tf ${UPD_FILE} | grep -q $file || retval=1 #echo "needed component '$file' not found in ${UPD_FILE}"
     done
     return $retval;
+}
+
+stage_check_current_version()
+{
+echo "stage_check_current_version()"
+retval=0
+
+ls -l ${EXEX_DIR_LINK}
+retvalue=$?
+if [[ $retvalue -ne 0 ]]
+then 
+    echo "stage_check_current_version() failed $retvalue"
+    return $retval;
+fi
+
+cur_filename=`ls -l ${EXEX_DIR_LINK} | awk '{ print $10; }' ${cur_filename} `
+cur_number=${cur_filename:0:12}
+echo $cur_filename
+echo $cur_number
+if [ $cur_number -gt 999999999999 ]
+then
+    echo grosser
+else
+    echo nicht groesser
+fi
+
+
+return $retval;
 }
 
 stage_search_stick()
