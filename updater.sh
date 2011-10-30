695baab5d485c651e155188475afe0f95a4da2c9
Bastian Ruppert <info@bastian-ruppert.de>
2011-10-30 13:38:39 +0100

    updater.sh: ln -fns erzeugt "anscheinend" den Link auf das Verzeichnis 	    korrekt!
    Signed-off-by: Bastian Ruppert <info@bastian-ruppert.de>

--- updater.sh
+++ updater.sh
@@ -1,56 +1,22 @@
 #!/bin/sh
 #
-# Prototype for software update script
-
-# script mit popen aus c-Programm aufrufen um auf die Ausgaben zugreifen zu können
+# Start und updatescript für capmb
 
 # Global definitions
 VERSION="2.0"
-#INSTALL_DIR="/opt/"
-TMP_USBSTICK=""
-
 #set -x
 
-
-die()
-{
-    echo "$*" >&2
-    #stage_clean
-    exit 1
-}
-
-# Helper functions
-
-# Implementation of individual stages
-stage_validate()
-{
-    echo "updater Version ${VERSION}"
-    echo "- validate"
-    tar xOf ${update_file} ${UPD_MD5SUMS} > ${TMP_MD5SUMS}
-    for file in ${COMPONENTS}; do
-        tar tf ${update_file} | grep -q $file || die "needed component '$file' not found in ${update_file}"
-        exp_sum=`awk '{ if ($2 == "'${file}'") print $1; }' ${TMP_MD5SUMS}`
-        sum=`tar xOf ${update_file} ${file} | md5sum | awk '{ print $1; }'`
-        if [ "$exp_sum" != "$sum" ]; then
-            die "md5sum mismatch for file '$file'"
-        fi
-    done
-    exit 0;
-}
-
-
-
-
 REGEXP='[0-9]\{12\}_capmb.tar.gz$'
-EXEX_DIR_LINK="capmb"
+EXEC_DIR_LINK="capmb"
 UPD_FILE_LEN=25
 UPD_DIR_LEN=18
 COMPONENTS="cap cap.conf v4l_capture.o appcontroller.sh"
 UPD_FILE="" #/media/SWAP/111016114939_capmb.tar.gz"
 UPD_DISK="/media/"
 UPD_DIR_NAME=""
-#UPDATE_STEPS="stage_search_stick stage_search_updatefile stage_check_updatefile"
-UPDATE_STEPS="stage_check_current_version"
+UPDATE_STEPS="stage_search_stick stage_search_updatefile stage_check_updatefile stage_check_current_version stage_copy_update"
+PROGRAMS_ROOT=""
+
 
 stage_update()
 {
@@ -74,12 +40,24 @@ stage_update()
 
 stage_copy_update()
 {
+    retval=0
     tar xfz ${UPD_FILE}
-#entpacken in pwd/../
-#link erzeugen in pwd/../
-#reboot oder script verlassen wenn das script mit respawn aufgerufen wird, dann muss allerdings
-# geprüft werden, ob das update neuer ist als das aktuell laufende Programm!!!
-# also ein reboot !
+
+    retvalue=$?
+    if [[ $retvalue -ne 0 ]]
+    then 
+	echo "stage_copy_update: unpack update failed $retvalue"
+	return $retval;
+    fi
+echo ln -fns ${PROGRAMS_ROOT}/${UPD_DIR_NAME} ${PROGRAMS_ROOT}/${EXEC_DIR_LINK}
+    ln -fns ${PROGRAMS_ROOT}/${UPD_DIR_NAME} ${PROGRAMS_ROOT}/${EXEC_DIR_LINK}
+    retvalue=$?
+    if [[ $retvalue -ne 0 ]]
+    then 
+	echo "stage_copy_update: set link failed $retvalue"
+	return $retval;
+    fi
+    return $retval
 }
 
 stage_check_updatefile()
@@ -95,10 +73,10 @@ retval=0
 
 stage_check_current_version()
 {
-echo "stage_check_current_version()"
 retval=0
 
-ls -l ${EXEX_DIR_LINK}
+#echo ls -l ${PROGRAMS_ROOT}/${EXEC_DIR_LINK}
+ls -l ${PROGRAMS_ROOT}/${EXEC_DIR_LINK}
 retvalue=$?
 if [[ $retvalue -ne 0 ]]
 then 
@@ -106,15 +84,19 @@ then
     return $retval;
 fi
 
-cur_filename=`ls -l ${EXEX_DIR_LINK} | awk '{ print $10; }' ${cur_filename} `
-cur_number=${cur_filename:0:12}
+cur_filename=`ls -l ${PROGRAMS_ROOT}/${EXEC_DIR_LINK} | awk '{ print $10; }' ${cur_filename} `
+cur_number=${cur_filename:${#cur_filename}-${UPD_DIR_LEN}-1:12}
+upd_number=${UPD_DIR_NAME:0:12}
 echo $cur_filename
 echo $cur_number
-if [ $cur_number -gt 999999999999 ]
+echo $upd_number
+if [ $upd_number -gt $cur_number ]
 then
-    echo grosser
+    echo found update ${upd_number} which is newer then ${cur_number}
+    return 0
 else
-    echo nicht groesser
+    echo no newer update found
+    return 1
 fi
 
 
@@ -139,7 +121,7 @@ retval=1
 line=`ls -1 $UPD_DISK`
 for file in $line
 do
-    filelen=${#file}
+    filelen=${#file} # die Länge eines Strings
     reg_match=`expr match "$file" $REGEXP`
     if [[  filelen -eq $UPD_FILE_LEN && reg_match -eq $UPD_FILE_LEN ]]
     then
@@ -147,92 +129,27 @@ do
 	#echo $file  
 	UPD_FILE=$UPD_DISK$file
 	UPD_DIR_NAME=${file:0:$UPD_DIR_LEN}
+	#echo stage_search_updatefile found updatefile: ${UPD_FILE}
     fi
 done
 return $retval
 }
 
-
+cd ..
+PROGRAMS_ROOT=`pwd`
+#real_directory=`ls -l ${EXEC_DIR_LINK} | awk '{ print $10; }' ${cur_filename} `
+#cd $real_directory
+#echo `pwd`
 
 stage_update
 retvalue=$?
-if [[ $retvalue -ne 0 ]]
+if [[ $retvalue -eq 0 ]]
 then 
-    echo "stage_update failed $retvalue"
-    OK=0
+    echo "reboot"
+    reboot
+    exit 0
 fi
+echo "stage_update failed $retvalue"
 echo "execute normal programm"
+
 exit 0
-
-# Update file is mandatory argument
-#if [ $# -lt 2 ]; then
-#    die
-#fi
-
-#update_file=$1
-#shift
-
-
-
-
-
-
-
-
-
-
-
-OK=1
-
-if [[ $OK -ne 0 ]]
-    then
-    stage_search_stick
-    retvalue=$?
-    if [[ $retvalue -ne 0 ]]
-    then 
-	echo "stage_search_stick failed $retvalue"
-	OK=0
-    fi
-fi
-
-if [[ $OK -ne 0 ]]
-    then
-    stage_search_updatefile
-    retvalue=$?
-    if [[ $retvalue -ne 0 ]]
-    then 
-	echo "stage_search_updatefile failed $retvalue"
-	OK=0
-    fi   
-fi
-
-if [[ $OK -ne 0 ]]
-    then
-    stage_check_updatefile
-    retvalue=$?
-    if [[ $retvalue -ne 0 ]]
-    then 
-	echo "check_updatefile failed $retvalue"
-	OK=0
-    fi   
-fi
-exit
-
-# When no stage is specified, do all of them
-if [ $# -eq 0 ]; then
-    echo "stage needed!"
-    exit -1
-fi
-
-while [ $# -gt 0 ]; do
-    case $1 in
-		 search_stick|search_updatefile|check_updatefile|copy_update|verify|copy)
-		     eval "stage_$1"
-		     ;;
-		 *)
-		     echo "unknown stage '$1'" >&2
-		     die
-		     ;;
-    esac
-    shift
-done
